b1c99872d1bdc8150c034b401b66fe321a7935b2
bulldoguk <gary@myhmbiz.com>
2022-09-05 21:34:49 -0500

    Finally got bundles pushing to mongo correctly

--- routers/bundles.py
+++ routers/bundles.py
@@ -1,10 +1,15 @@
 # user.py
 
 from modules.db.mongodb import get_db_mumshoppe
-from typing import Optional, Tuple
+from pymongo import ReturnDocument
+
+import uuid
+
+from typing import Optional
 
 from fastapi import APIRouter, Response, status
 from pydantic import BaseModel
+
 
 class Credit(BaseModel):
     optionid: str
@@ -13,7 +18,7 @@ class Credit(BaseModel):
 
 
 class Bundle(BaseModel):
-    id: Optional[str] = None
+    guid: Optional[str] = None
     type: str
     size: str
     description: str
@@ -26,19 +31,37 @@ router = APIRouter(
     tags=["bundles"],
     responses={
         404: {"description": "Not found"},
-        401: {"description": "Invalid request"}
+        400: {"description": "Invalid request"}
     }
 )
 
 
-@router.get("/list")
+@router.get("/list", status_code=status.HTTP_200_OK)
 async def get_bundles():
     return {"name": 'Gary'}
 
 
-@router.post("/", status_code=200)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def add_bundle(bundle: Bundle, response: Response):
-    if bundle.id:
-        response.status_code = status.HTTP_400_E
-        return "Invalid request"
+    try:
+        event_collection = get_db_mumshoppe().bundles
+        if not bundle.guid:
+            bundle.guid = str(uuid.uuid4())
+        record = event_collection.find_one_and_update(
+            {"guid": bundle.guid},
+            {"$set": bundle.dict()},
+            upsert=True,
+            return_document=ReturnDocument.AFTER)
+        del record["_id"]
+        return record
+    except Exception as e:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"Error": e}
+
+
+@router.patch("/", status_code=status.HTTP_202_ACCEPTED)
+async def update_bundle(bundle: Bundle, response: Response):
+    if not bundle.id:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return "Bundle ID is missing"
     return bundle
