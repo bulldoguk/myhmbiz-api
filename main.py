59f1b6741f9c7b1d1138e3c5288318459a2e2b18
bulldoguk <gary@myhmbiz.com>
2022-09-06 17:12:42 -0500

    API key not required in dev environment

--- main.py
+++ main.py
@@ -3,8 +3,12 @@
 from fastapi import FastAPI, Depends
 from routers import user, bundles, options, orders, shoppes
 from modules.apikey import checkkey
+from decouple import config
 
-app = FastAPI(dependencies=[Depends(checkkey.verify_key)])
+if not config('DEVELOPMENT'):
+    app = FastAPI(dependencies=[Depends(checkkey.verify_key)])
+else:
+    app = FastAPI()
 
 # Common
 app.include_router(user.router)
--- modules/apikey/checkkey.py
+++ modules/apikey/checkkey.py
@@ -2,5 +2,5 @@ from fastapi import Header, HTTPException
 from decouple import config
 
 async def verify_key(x_key: str = Header()):
-    if x_key != config('APIKEY'):
+    if x_key != config('APIKEY') and not config('DEVELOPMENT'):
         raise HTTPException(status_code=403, detail='Unauthorized access')
