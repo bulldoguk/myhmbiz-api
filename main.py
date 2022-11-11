39d506b1dce89ffee56336d17bde0ef7da415ae2
bulldoguk <gary@myhmbiz.com>
2022-11-10 18:55:53 -0600

    Adding uvicorn to main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routers import user, bundles, options, orders, shoppes
 from modules.apikey import checkkey
 from decouple import config
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 if not config('DEVELOPMENT'):
     app = FastAPI(dependencies=[Depends(checkkey.verify_key)])
@@ -37,3 +38,7 @@ app.include_router(orders.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
