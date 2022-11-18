05f6b5975b2d6a6c60543a6f76f26472a7cd6c9e
bulldoguk <gary@myhmbiz.com>
2022-11-17 19:28:45 -0600

    Need to rework DB connections, but when I can connect to mongodb

--- modules/apikey/checkkey.py
+++ modules/apikey/checkkey.py
@@ -2,5 +2,6 @@ from fastapi import Header, HTTPException
 from decouple import config
 
 async def verify_key(x_key: str = Header()):
+    print(f'Getting header key: {x_key}')
     if x_key != config('APIKEY') and not config('DEVELOPMENT'):
         raise HTTPException(status_code=403, detail='Unauthorized access')
